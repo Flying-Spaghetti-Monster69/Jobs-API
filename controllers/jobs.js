97e050e3f301f109653aa71dc0582c47efe90861
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-01-04 19:42:29 -0500

    jobs controller code added

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,23 +1,57 @@
-
+const { StatusCodes } = require("http-status-codes")
+const Job = require('../models/Job')
+const {BadRequestError, NotFoundError} = require('../errors')
 
 const getAllJobs = async (req,res)  =>{
-    res.send('get all jobs')
+    const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+    res.status(StatusCodes.OK).json({jobs, nHits:jobs.length})
 }
 
 const getJob = async (req,res)  =>{
-    res.send('get job')
+    const {user:{userId},params:{id:jobId}} = req
+
+    const job = await Job.findOne({_id:jobId, createdBy:userId})
+
+    if (!job) {
+        throw new NotFoundError(`job not found with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
 }
 
 const createJob = async (req,res)  =>{
-    res.json(req.user)
+    req.body.createdBy = req.user.userId;
+    const job = await Job.create(req.body)
+    res.status(StatusCodes.CREATED).json({job})
 }
 
 const updateJob = async (req,res)  =>{
-    res.send('update job')
+    const {user:{userId},params:{id:jobId},body:{company, position}} = req
+
+    if (company===''||position==='') {
+        throw new BadRequestError('company and position can not be empty')
+    }
+
+    const job = await Job.findOneAndUpdate({_id:jobId, createdBy:userId},req.body,
+        {new:true,runValidators:true})
+
+    if (!job) {
+        throw new NotFoundError(`job not found with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
 }
 
 const deleteJob = async (req,res)  =>{
-    res.send('delete job')
+    const {user:{userId},params:{id:jobId}} = req
+
+    const job = await Job.findOneAndRemove({_id:jobId, createdBy:userId})
+
+    if (!job) {
+        throw new NotFoundError(`job not found with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).send("deleted successfully")
 }
 
 module.exports = {
--- models/Job.js
+++ models/Job.js
@@ -3,19 +3,29 @@ const moongose = require('mongoose')
 
 const jobSchema = new moongose.Schema({
     company:{
-        type:string,
+        type:String,
         required:[true, 'please provide company name'],
         maxlenght:69
     },
 
     position:{
-        type:string,
+        type:String,
         required:[true, 'please provide company name'],
         maxlenght:420
     },
 
     status:{
-        type:string,
-        enum:['interview','declined','pending']
+        type:String,
+        enum:['interview','declined','pending'],
+        deafult: 'pending'
+    },
+
+    createdBy:{
+        type:moongose.Types.ObjectId,
+        ref:'User',
+        required:[true,'please provide a user']
     }
-})
+
+},{timestamps:true})
+
+module.exports = moongose.model('Job',jobSchema) 
